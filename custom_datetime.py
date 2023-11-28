6479b5595f73c29f183c430621b40d5048a67f11
Krishna Teja Rangavajjala <64663804+KrishnaTeja-2002@users.noreply.github.com>
2023-11-28 14:47:24 -0600

    Update custom_datetime.py

--- custom_datetime.py
+++ custom_datetime.py
@@ -1,5 +1,7 @@
-# custom_datetime.py
-class CustomDatetime:
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+class CustomDatetime(ABC):
     def __init__(self, year, month, day, hour=0, minute=0, second=0):
         self.year = year
         self.month = month
@@ -13,3 +15,37 @@ class CustomDatetime:
 
     def to_human_readable_format(self):
         return f"{self.month:02d}/{self.day:02d}/{self.year} {self.hour:02d}:{self.minute:02d}:{self.second:02d}"
+
+    @staticmethod
+    def static_method():
+        # Actual content of your static method here
+        pass
+
+    @abstractmethod
+    def abstract_method(self):
+        # Actual implementation of your abstract method here
+        pass
+
+    @staticmethod
+    def validate_date(year, month, day):
+        # Static method for date validation
+        if 1 <= month <= 12 and 1 <= day <= 31:
+            return True
+        else:
+            raise ValueError("Invalid date")
+
+    def custom_exception_handling(self):
+        try:
+            # Actual code that might raise an exception
+            result = self.year / (self.month - 12)
+            return result
+        except Exception as e:
+            # Handle the exception and provide an informative error message
+            return f"Error: {str(e)}"
+
+
+# Example subclass implementing the abstract method
+class CustomDatetimeSubclass(CustomDatetime):
+    def abstract_method(self):
+        # Your implementation of the abstract method
+        pass
