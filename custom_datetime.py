683e76d1d6dd4ec4fdbc54e20ef0b724a3f43a34
Krishna Teja Rangavajjala <64663804+KrishnaTeja-2002@users.noreply.github.com>
2023-11-28 22:50:56 -0600

    Update custom_datetime.py

--- custom_datetime.py
+++ custom_datetime.py
@@ -1,67 +1,105 @@
-from abc import ABC, abstractmethod
-from datetime import datetime
+# custom_datetime.py
+import datetime
 
-class CustomDatetime(ABC):
-    def __init__(self, year, month, day, hour=0, minute=0, second=0):
-        self.year = year
-        self.month = month
-        self.day = day
-        self.hour = hour
-        self.minute = minute
-        self.second = second
+class CustomDatetime:
 
-    def to_iso_format(self):
-        result = f"{self.year}-{self.month:02d}-{self.day:02d}T{self.hour:02d}:{self.minute:02d}:{self.second:02d}"
-        print("to_iso_format:", result)
-        return result
+    def __init__(self, year=None, month=None, day=None, hour=None, minute=None, second=None):
+     
+      
 
-    def to_human_readable_format(self):
-        result = f"{self.month:02d}/{self.day:02d}/{self.year} {self.hour:02d}:{self.minute:02d}:{self.second:02d}"
-        print("to_human_readable_format:", result)
-        return result
-
-    @staticmethod
-    def static_method():
-        result = "Static method executed."
-        print("static_method:", result)
-        return result
+        if year is None and month is None and day is None and hour is None and minute is None and second is None:
+            current_datetime = datetime.datetime.utcnow()
+            self.year = current_datetime.year
+            self.month = current_datetime.month
+            self.day = current_datetime.day
+            self.hour = current_datetime.hour
+            self.minute = current_datetime.minute
+            self.second = current_datetime.second
+        else:
+            self.year = year or 0
+            self.month = month or 0
+            self.day = day or 0
+            self.hour = hour or 0
+            self.minute = minute or 0
+            self.second = second or 0
 
     @classmethod
-    def class_method_example(cls):
-        result = f"Class method executed for {cls.__name__}."
-        print("class_method_example:", result)
-        return result
-
-    @abstractmethod
-    def abstract_method(self):
-        result = "Abstract method executed."
-        print("abstract_method:", result)
-        return result
+    def from_iso8601(cls, iso8601_string):
+        
+        try:
+            datetime_obj = datetime.datetime.fromisoformat(iso8601_string)
+            return cls(datetime_obj.year, datetime_obj.month, datetime_obj.day,
+                       datetime_obj.hour, datetime_obj.minute, datetime_obj.second)
+        except ValueError:
+            raise ValueError(f"Invalid ISO 8601 format: {iso8601_string}")
 
     @staticmethod
-    def validate_date(year, month, day):
-        # Static method for date validation
-        if 1 <= month <= 12 and 1 <= day <= 31:
-            print("validate_date: Valid date.")
-            return True
-        else:
-            raise ValueError("validate_date: Invalid date")
+    def is_valid_date(year, month, day):
+        """
+        Checks whether the given set of date arguments (day, month, year) forms a valid date.
 
-    def custom_exception_handling(self):
+        Args:
+            year (int): The year of the date.
+            month (int): The month of the date (1-12).
+            day (int): The day of the month (1-31).
+
+        Returns:
+            bool: True if the given date is valid, False otherwise.
+        """
+
         try:
-            # Actual code that might raise an exception
-            result = self.year / (self.month - 12)
-            print("custom_exception_handling:", result)
-            return result
-        except Exception as e:
-            # Handle the exception and provide an informative error message
-            result = f"custom_exception_handling: Error - {str(e)}"
-            print(result)
-            return result
+            datetime.datetime(year, month, day)
+            return True
+        except ValueError:
+            return False
 
-# Example subclass implementing the abstract method
-class CustomDatetimeSubclass(CustomDatetime):
-    def abstract_method(self):
-        result = "CustomDatetimeSubclass abstract method executed."
-        print("CustomDatetimeSubclass abstract_method:", result)
-        return result
+    def to_iso8601(self):
+        return datetime.datetime(
+            self.year, 
+            self.month,
+            self.day,
+            self.hour,
+            self.minute, 
+            self.second
+        ).isoformat()
+
+    def to_human_readable(self):
+        
+
+        return f"{self.year}-{self.month}-{self.day} {self.hour}:{self.minute}:{self.second}"
+
+    @classmethod
+    def validate_date(cls, year, month, day):
+       
+        try:
+            datetime.datetime(year, month, day)
+            return True
+        except ValueError:
+            return False
+
+    @classmethod
+    def date_difference(cls, date1, date2, unit='days'):
+       
+        if not isinstance(date1, cls) or not isinstance(date2, cls):
+            raise ValueError("Both arguments must be instances of the Datetime class.")
+
+        delta = abs(date1.to_datetime() - date2.to_datetime())
+
+        if unit == 'days':
+            return delta.days
+        elif unit == 'weeks':
+            return delta.days // 7
+        elif unit == 'months':
+            return delta.days // 30
+            
+    def __repr__(self):
+        return f"CustomDatetime(year={self.year}, month={self.month}, day={self.day}, " \
+               f"hour={self.hour}, minute={self.minute}, second={self.second})"
+
+    @classmethod
+    def from_string(cls, date_string):
+        
+        try:
+            date_obj = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+            return cls(date_obj.year, date_obj.month, date_obj.day,
+                       date_obj.hour, date_obj.minute, date
